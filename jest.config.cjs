70e5d9bf1d72ff7f93c709883e1846bd5f304841
Uriel Bertoche <uriel@bertoche.com.br>
2025-12-17 16:29:34 -0300

    fixing errors due to incompatible versions

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,8 +1,11 @@
 module.exports = {
     globals: {},
     testEnvironment: 'jsdom',
+    testEnvironmentOptions: {
+        customExportConditions: ['node', 'node-addons']
+    },
     transform: {
-        '^.+\\.vue$': 'vue-jest',
+        '^.+\\.vue$': '@vue/vue3-jest',
         '^.+\\.js$': 'babel-jest'
     },
     moduleFileExtensions: ['vue', 'js', 'json', 'jsx', 'ts', 'tsx', 'node']
